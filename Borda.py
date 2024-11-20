a59c9dbb23836550f10860783090c7c9fe45b553
TeancumDev <teancum.price.dev@gmail.com>
2024-11-20 15:09:13 -0700

    Borda Winner finder
    Basic function to determine a borda winner given a score vector and a population

--- Borda.py
+++ Borda.py
@@ -2,10 +2,20 @@ CERVONE_CONSTANT = 1.42554
 
 def generateBordaVector(type, candidates, power):
     if type=="linear":
-        borda = [((candidates-i)/(candidates-1))**power for i in range(1, candidates+1)]
-        return borda
+        bordaVector = [((candidates-i)/(candidates-1))**power for i in range(1, candidates+1)]
+        return bordaVector
     elif type=="inverse":
-        borda = [((candidates-i)/(i*(candidates-1)))**power for i in range(1, candidates+1)]
-        return borda
+        bordaVector = [((candidates-i)/(i*(candidates-1)))**power for i in range(1, candidates+1)]
+        return bordaVector
     else:
         return None
+    
+def findBordaWinner(scoreVector, candidates, population):
+    scores = [0] * candidates
+    for scoreTuple in population:
+        order = scoreTuple[0]
+        groupSize = scoreTuple[1]
+        for i in range(candidates):
+            scores[order[i]] += scoreVector[i] * groupSize
+    winner = max([j for j in range(candidates)], key=lambda k: scores[k])
+    print("The winner is:", winner)
