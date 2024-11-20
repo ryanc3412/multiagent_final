ad99af1fdf975b7a2e7ff05dbc2cf564ad74c3a4
TeancumDev <teancum.price.dev@gmail.com>
2024-11-20 13:04:32 -0700

    Borda Vector Generation
    Created a function to generate the borda vectors.  Will add more features to the file later

---
+++ Borda.py
@@ -0,0 +1,9 @@
+def generateBordaVector(type, candidates, power):
+    if type=="linear":
+        borda = [((candidates-i)/(candidates-1))**power for i in range(1, candidates+1)]
+        return borda
+    elif type=="inverse":
+        borda = [((candidates-i)/(i*(candidates-1)))**power for i in range(1, candidates+1)]
+        return borda
+    else:
+        return None
