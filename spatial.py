c3f82b617bbc33308b6d31b61cc5fa16747dbcf7
TeancumDev <teancum.price.dev@gmail.com>
2024-12-08 17:43:53 -0700

    Updated Spatial.py
    Changed the input and output of spatial.py so that they were the same as for IC and IAC

--- spatial.py
+++ spatial.py
@@ -1,8 +1,8 @@
 import numpy as np
+from itertools import permutations
 
-def spatial(num_voters, candidates):
-
-    num_candidates = len(candidates)
+def spatial(num_voters, num_candidates):
+    candidates=[i for i in range(num_candidates)]
 
     voter_positions = np.random.rand(num_voters)
     candidate_positions = np.random.rand(num_candidates)
@@ -13,6 +13,14 @@ def spatial(num_voters, candidates):
 
         distances = np.abs(candidate_positions - voter_pos)
         ranking = [candidates[i] for i in np.argsort(distances)]
-        voter_rankings.append(ranking)
+        voter_rankings.append(tuple(ranking))
     
-    return voter_rankings
+    #Convert the voter rankings to the same format as the other populations
+    perms = list(permutations(candidates))
+    permutation_count = [0 for i in range(len(perms))]
+    for voter in voter_rankings:
+        index = perms.index(voter)
+        permutation_count[index] +=1
+    result = [(perms[i], permutation_count[i]) for i in range(len(perms))]
+
+    return result
